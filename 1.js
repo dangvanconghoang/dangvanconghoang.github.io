16fb7bc2ec304743c39a8982a0c4c1ef40ce9fc8
DANG VAN CONG HOANG <hoangdang@hoangdangs-MacBook-Pro.local>
2018-11-04 18:53:08 +0700

    sdfsdfds

---
+++ 1.js
@@ -0,0 +1,18 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+
+ 		if($('html,body').scrollTop()>300) {
+ 			$('.nonecolor').addClass('whitebar');
+ 			$('.nutlen').addClass('hienthi')
+ 		}
+ 		else if($('html,body').scrollTop()<=100) {
+			$('.nonecolor').removeClass('whitebar');
+			$('.nutlen').removeClass('hienthi');
+ 		}
+ 	})
+ 	$('.nutlen').click(function(){
+ 		$('html,body').animate({'scrollTop':0});
+ 		 return false;
+ 	})
+})  
+ 
